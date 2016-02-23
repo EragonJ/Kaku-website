92536b84fa6fa0c7ca84093a39eb023dfd95cfc4
Peng-Jie <dean.leehom@gmail.com>
2016-02-23 10:37:05 +0800

    use tutorial json parse title

--- public/js/list-qa.js
+++ public/js/list-qa.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  // TODO
+  $.ajax({
+    url: '../tutorial.json'
+  })
+  .done(function(data) {
+    var objLength = Object.keys(data).length;
+
+    for (var i = 0; i < objLength; i++) {
+      $('#en').append('<a href="en/how-to-back-up-music.html" class="list-group-item">' + data[i].en + '</a>');
+      $('#tw').append('<a href="tw/how-to-back-up-music.html" class="list-group-item">' + data[i]['zh-TW'] + '</a>');
+    }
+  });
 });
