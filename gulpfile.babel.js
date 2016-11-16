9340211bdf1d06182d76f1f4a8d9087d04e26883
EragonJ <eragonj@eragonj.me>
2016-11-16 23:07:40 +0800

    tidied things up and moved them into src/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,6 @@ import uglify from 'gulp-uglify';
 import fileinclude from 'gulp-file-include';
 
 const supportLanguages = ['en', 'zh-TW'];
-const vendor = `./public/vendor`;
 
 function merge(lang, file, tmpDocFiles) {
   const str = file.replace('.html', '');
@@ -17,7 +16,7 @@ function merge(lang, file, tmpDocFiles) {
   const content = fs.readFileSync(`${tmpDocFiles}${file}`, 'utf8');
 
   return gulp
-    .src('./docs/_doc.html')
+    .src('src/docs/_doc.html')
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@root'
@@ -31,18 +30,23 @@ function merge(lang, file, tmpDocFiles) {
 }
 
 gulp.task('en', (cb) => {
-  const docsFiles = `./docs/src/en/`;
-  const files = fs.readdirSync(docsFiles);
-
   return gulp
-    .src('./docs/src/en/*.md')
+    .src('src/docs/en/*.md')
     .pipe(markdown())
     .pipe(gulp.dest(`./docs/tmp/en`));
     cb();
 });
 
+gulp.task('zhTW', (cb) => {
+  return gulp
+    .src('src/docs/zh-TW/*.md')
+    .pipe(markdown())
+    .pipe(gulp.dest(`./docs/tmp/zh-TW`));
+    cb();
+});
+
 gulp.task('mergeEn', ['en'], () => {
-  const tmpDocFiles = `./docs/tmp/en/`;
+  const tmpDocFiles = `docs/tmp/en/`;
   const mergeDocs = fs.readdirSync(tmpDocFiles);
 
   mergeDocs.map((file) => {
@@ -50,19 +54,8 @@ gulp.task('mergeEn', ['en'], () => {
   });
 });
 
-gulp.task('zhTW', (cb) => {
-  const docsFiles = `./docs/src/zh-TW/`;
-  const files = fs.readdirSync(docsFiles);
-
-  return gulp
-    .src('./docs/src/zh-TW/*.md')
-    .pipe(markdown())
-    .pipe(gulp.dest(`./docs/tmp/zh-TW`));
-    cb();
-});
-
 gulp.task('mergeTW', ['zhTW'], () => {
-  const tmpDocFiles = `./docs/tmp/zh-TW/`;
+  const tmpDocFiles = `docs/tmp/zh-TW/`;
   const mergeDocs = fs.readdirSync(tmpDocFiles);
 
   mergeDocs.map((file) => {
@@ -72,7 +65,7 @@ gulp.task('mergeTW', ['zhTW'], () => {
 
 gulp.task('docIndex', () => {
   return gulp
-    .src('./docs/_index.html')
+    .src('src/docs/_index.html')
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@root'
@@ -83,7 +76,7 @@ gulp.task('docIndex', () => {
 
 gulp.task('buildIndex', () => {
   return gulp
-    .src('./_index.html')
+    .src('src/_index.html')
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@root'
@@ -92,8 +85,15 @@ gulp.task('buildIndex', () => {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('buildDocs', ['mergeEn', 'mergeTW', 'docIndex'], (cb) => {
+gulp.task('buildDocs', [
+  'mergeEn',
+  'mergeTW',
+  'docIndex'
+], (cb) => {
   rimraf('./docs/tmp', cb);
 });
 
-gulp.task('default', ['buildDocs', 'buildIndex']);
+gulp.task('default', [
+  'buildDocs',
+  'buildIndex'
+]);
